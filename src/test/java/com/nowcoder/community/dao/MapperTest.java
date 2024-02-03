6dd566b3e7b19de965ba6d53860cad9ece0f6a83
Yuu <2087791511@qq.com>
2024-02-03 20:59:58 +0800

    first test

---
+++ src/test/java/com/nowcoder/community/dao/MapperTest.java
@@ -0,0 +1,68 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.CommunityApplication;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+import java.util.List;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MapperTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+
+    @Test
+    public void testSelectUser(){
+        User user = userMapper.selectById(111);
+        System.out.println(user);
+
+        User user1 = userMapper.selectByName("liubei");
+        System.out.println(user1);
+
+        User user2 = userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user2);
+    }
+
+    @Test
+    public void testInsertUser(){
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abc");
+        user.setEmail("test@qq.com");
+        user.setHeaderUrl("http://www.nowcoder.com/101.png");
+        user.setCreateTime(new Date());
+
+        int rows = userMapper.insertUser(user);
+        System.out.println(rows);
+        System.out.println(user.getId());
+    }
+
+    @Test
+    public void testSelectPosts(){
+        List<DiscussPost> list = discussPostMapper.selectDiscussPosts(149, 0, 10);
+        for(DiscussPost post:list){
+            System.out.println(post);
+        }
+
+        int rows = discussPostMapper.selectDiscussPostRows(149);
+        System.out.println(rows);
+    }
+
+
+
+}
