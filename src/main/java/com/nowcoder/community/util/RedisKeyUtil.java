e2e3607e88c1c8935e8be2ac828ee67db02f5c61
Yuu <2087791511@qq.com>
2024-02-22 16:27:34 +0800

    整合redis

---
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -0,0 +1,8 @@
+package com.nowcoder.community.util;
+
+public class RedisKeyUtil {
+
+    private static final String SPLIT = ":";
+    private static final String PREFIX_ENTITY_LIKE = "like:entity";
+
+}
