c99fa275b89602abb8b08b4665161a55d32eef02
Yuu <2087791511@qq.com>
2024-03-12 21:42:15 +0800

    优化网站性能

---
+++ src/test/java/com/nowcoder/community/CaffeineTests.java
@@ -0,0 +1,25 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.service.DiscussPostService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+@RunWith(SpringRunner.class)
+public class CaffeineTests {
+
+    @Autowired
+    private DiscussPostService postService;
+
+    @Test
+    public void testCache(){
+        System.out.println(postService.findDiscussPosts(0, 0 , 10 ,1));
+        System.out.println(postService.findDiscussPosts(0, 0 , 10 ,1));
+        System.out.println(postService.findDiscussPosts(0, 0 , 10 ,1));
+    }
+}
